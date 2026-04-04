e66b99996108f0d401deeb672bc9d7ab47d55c58
root <root@picoctf>
2026-04-04 02:01:04 -0700

    Querying for events kind of works except for details, and maybe timestamp but that isnt checked

--- app/routes/events.py
+++ app/routes/events.py
@@ -9,6 +9,15 @@ from app.models.events import Events
 
 events_bp = Blueprint("events", __name__)
 
+QUERY_FIELDS = {
+    "id": Events.id,
+    "url_id": Events.url_id,
+    "user_id": Events.user_id,
+    "event_type": Events.event_type,
+    "timestamp": Events.timestamp,
+    "details": Events.details
+}
+
 # This helper is designed the handle all the misformatting that
 # might be done while trying to parse certain values from the
 # Events class
@@ -32,13 +41,47 @@ def prepare_values(event_val):
 
     return event_val # The event with processed values or the unchanged if none of the keys needed it
 
-@events_bp.route("/events")
-def list_events():
+            
+# This lets me build a list of filters that will be used when
+# searching the events database, as far as i know this will
+# be abe to cover all the post requests(if they even exist)
+# QUERY_FIELDS:
+# See top of file
+def build_search_list(query_json):
+    if len(query_json) <= 0:
+        return jsonify({"error": "No post body was provided"}), 400
+    else:
+        filters = []
+        for key, value in query_json.items():
+            filter = QUERY_FIELDS.get(key)
+            if not filter or not value:
+                continue
+            filters.append(filter == value)
+        return filters
+    return jsonify({"error": "There was a problem with handling the request"}), 400
 
+
+# This will get the values from the event db that correspond
+# to the filter provided in the post body
+def get_events_filtered(json_query):
+    filters = build_search_list(json_query)
+    if filters is None:
+        return jsonify({"error": "No post body was provided"}), 400 # again not sure but wasnt comfortable leaving empty
+    if len(filters) <= 0:
+        return jsonify({"error": "Query parameters invalid."}), 400 # all these need to be changed with new logging setup i think
+    query = Events.select().dicts()
+    result = query.where(*filters)
+    cleaned_result = [prepare_values(r) for r in result]
+    return jsonify(cleaned_result)
+
+
+
+@events_bp.route("/events", methods=['GET', 'POST'])
+def list_events():
     if request.method == 'GET':
         try:
             events = Events.select().dicts()
-
+            # Cant use model_to_dict because it doesnt handle some values properly(ex. details or timestamps)
             json_list = []
             for conv_dict in events:
                 if conv_dict is None:
@@ -55,4 +98,15 @@ def list_events():
             return jsonify({"error": f"Internal Error: {e}"}), 500
 
     if request.method == 'POST':
-        print("Some post logic")
+        try:
+            query = request.get_json()
+            if query is None:
+                return jsonify({"error": "error"}), 400 # dont know what to do here because maybe empty post shoudl return something
+            else: # Writing explicitly for clarity from comment above
+                query_response = get_events_filtered(query)
+                return query_response
+        except Exception as e:
+            return jsonify({"error": f"Internal Erorr: {e}"}), 500
+            
+
+    return 1
