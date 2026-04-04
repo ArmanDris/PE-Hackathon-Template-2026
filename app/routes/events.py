69f6abbaebaa5d5bacb11f6ea15babed3a113298
root <root@picoctf>
2026-04-04 14:33:29 -0700

    GET /events now accepts query parameters and should work for everything, except maybe not 'details' im working on getting the order back to match postgres column order

--- app/routes/events.py
+++ app/routes/events.py
@@ -42,9 +42,6 @@ def prepare_values(event_val):
     return event_val # The event with processed values or the unchanged if none of the keys needed it
 
 def build_search_list(query_json):
-    if len(query_json) <= 0:
-        return jsonify({"error", "No post body was provided"}), 400
-
     filters = []
     for key, value, in query_json.items():
         field = QUERY_FIELDS.get(key)
@@ -61,52 +58,56 @@ def build_search_list(query_json):
 
     return filters
 
-# This will get the values from the event db that correspond
-# to the filter provided in the post body
-def get_events_filtered(json_query):
-    filters = build_search_list(json_query)
-    if filters is None:
-        return jsonify({"error": "No post body was provided"}), 400 # again not sure but wasnt comfortable leaving empty
-    if len(filters) <= 0:
-        return jsonify({"error": "Query parameters invalid."}), 400 # all these need to be changed with new logging setup i think
-    query = Events.select().where(*filters).dicts()
-    #result = query.where(*filters)
+# Will filter the Events based on query param, if none are provided it will return the whole list
+def get_events_filtered(query_param):
+    print("QUery params: ", query_param)
+    try :
+        filters = build_search_list(query_param)
+    except Exception as e:
+        return jsonify({"error": f"Filter build error"}), 500
+        
+    query = Events.select()
+
+    try :
+        if filters:
+            query = query.where(*filters)
+    except:
+        return jsonify({"error": "There was a problem while filtering the events"}), 500
+
+    query = query.dicts()
+
     cleaned_result = [prepare_values(r) for r in query]
     return jsonify(cleaned_result)
-
 
 
 @events_bp.route("/events", methods=['GET', 'POST'])
 def list_events():
     if request.method == 'GET':
-        try:
-            events = Events.select().dicts()
-            # Cant use model_to_dict because it doesnt handle some values properly(ex. details or timestamps)
-            json_list = []
-            for conv_dict in events:
-                if conv_dict is None:
-                    continue
-                json_list.append(prepare_values(conv_dict))
 
-            #This is the only way i could figure out how to keep the order. Otherwise jsonify is much simpler
-            return Response(
-                json.dumps(json_list, indent=2, default=str),
-                mimetype="application/json"
-            )
+        query = request.args.to_dict()
+        try:
+            filtered_response = get_events_filtered(query)
+            return filtered_response
+
+            # Legacy response used to format properly, might still need to be used
+            #return Response(
+            #    json.dumps(json_list, indent=2, default=str),
+            #    mimetype="application/json"
+            #)
         except Exception as e:
             # Inspired changes, apparently this can happen if there is something wrong with the db
             return jsonify({"error": f"Internal Error: {e}"}), 500
 
-    if request.method == 'POST':
-        try:
-            query = request.get_json()
-            if query is None:
-                return jsonify({"error": "error"}), 400 # dont know what to do here because maybe empty post shoudl return something
-            else: # Writing explicitly for clarity from comment above
-                query_response = get_events_filtered(query)
-                return query_response
-        except Exception as e:
-            return jsonify({"error": f"Internal Erorr: {e}"}), 500
-            
+        #try:
+        #    query = request.get_json()
+        #    if query is None:
+        #        return jsonify({"error": "error"}), 400 # dont know what to do here because maybe empty post shoudl return something
+        #    else: # Writing explicitly for clarity from comment above
+        #        query_response = get_events_filtered(query)
+        #        return query_response
+        #except Exception as e:
+        #    return jsonify({"error": f"Internal Erorr: {e}"}), 500
 
+    if request.method == 'POST':
+        return jsonify({"error:": "Be kind to all"}), 418
     return 1
